da38ee0bcc5455335c7ede4e73358f4f685c1edd
liuyang <1714610279@qq.com>
2021-10-19 13:56:14 +0800

    feat(navbar): navbar添加全屏切换按钮

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -2,6 +2,9 @@
   <div class="navbar">
     <hambuger @toggleClick="toggleSidebar" :is-active="sidebar.opened"/>
     <breadcrumb/>
+    <div class="right-menu">
+      <screenfull id="screenfull" class="right-menu-item hover-effect"></screenfull>
+    </div>
   </div>
 </template>
 
@@ -10,12 +13,14 @@ import { computed, defineComponent } from 'vue'
 import Breadcrumb from '@/components/Breadcrumb/index.vue'
 import Hambuger from '@/components/Hambuger/index.vue'
 import { useStore } from '@/store'
+import Screenfull from '@/components/Screenfull/index.vue'
 
 export default defineComponent({
   name: 'Navbar',
   components: {
     Hambuger,
-    Breadcrumb
+    Breadcrumb,
+    Screenfull
   },
   setup () {
     const store = useStore()
@@ -31,3 +36,21 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+
+  .right-menu {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 15px;
+
+    .right-menu-item.hover-effect {
+      cursor: pointer;
+    }
+  }
+}
+</style>
